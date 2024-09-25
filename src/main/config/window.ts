02a29ffa5786595a4fae11a39b80235df09d7eb3
duanyongcheng77 <duanyongcheng77@gmail.com>
2024-09-25 14:11:46 +0800

    feat: 🎸 add recat router && add config pages

--- src/main/config/window.ts
+++ src/main/config/window.ts
@@ -2,6 +2,7 @@ import { BrowserWindow, shell } from 'electron'
 import { join } from 'path'
 import { is } from '@electron-toolkit/utils'
 import icon from '../../../resources/icon.png?asset'
+import url from 'node:url'
 
 export function createWindow(): BrowserWindow {
   // Create the browser window.
@@ -31,9 +32,20 @@ export function createWindow(): BrowserWindow {
   // HMR for renderer base on electron-vite cli.
   // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'] + '/#/config')
   } else {
-    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+    mainWindow.loadURL(
+      url.format({
+        //编译后的文件
+        pathname: join(__dirname, '../renderer/index.html'),
+        //协议
+        protocol: 'file',
+        //protocol 后面需要两个/
+        slashes: true,
+        //hash 的值
+        hash: 'config'
+      })
+    )
   }
   return mainWindow
 }
